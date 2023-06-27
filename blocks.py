8bf3dd909aafbfb5a7ca0eb4a5cfbe273acb522b
clissa <clissa.professional@gmail.com>
2023-06-27 11:49:05 +0200

    Implement c-ResUnet with pre-trained weights from Morelli, R., Clissa, L. et al., SciRep (2021)

--- blocks.py
+++ blocks.py
@@ -1,6 +1,10 @@
+from pathlib import Path
 from fastai.vision.all import *
 import fastai
+from utils import load_pkl, pt2k_state_dict, transfer_weights
 
+
+MODELS_PATH = Path().cwd() / "models" # edit here with path to pre-trained weights
 
 def get_layer_name(layer, idx):
     if isinstance(layer, torch.nn.Conv2d):
@@ -222,3 +226,56 @@ class cResUnet(nn.Module):
 
     def forward(self, x: Tensor) -> Tensor:
         return self._forward_impl(x)
+
+
+
+def _resunet(
+    arch: str,
+    n_features_start: int,
+    n_out: int,
+    #     block: Type[Union[BasicBlock, Bottleneck]],
+    #     layers: List[int],
+    pretrained: bool,
+    progress: bool,
+    **kwargs,
+) -> cResUnet:
+    model = cResUnet(n_features_start, n_out)  # , **kwargs)
+    model.__name__ = arch
+    # TODO: implement weights fetching if not present
+    if pretrained:
+        weights_path = MODELS_PATH / f"{arch}_state_dict.pkl"
+        print("loading pretrained Keras weights from", weights_path)
+        keras_weights = load_pkl(weights_path)
+        keras_state_dict = pt2k_state_dict(model.state_dict())
+        assert len(keras_weights) == len(keras_state_dict)
+        transfer_weights(model, keras_weights, keras_state_dict)
+    #         state_dict = load_state_dict_from_url(model_urls[arch], progress=progress)
+    #         model.load_state_dict(state_dict)
+    else:
+        model.init_kaiming_normal()
+    return model
+
+
+def c_resunet(
+    arch="c-ResUnet",
+    n_features_start: int = 16,
+    n_out: int = 2,
+    pretrained: bool = False,
+    progress: bool = True,
+    **kwargs,
+) -> cResUnet:
+    r"""cResUnet model from `"Automating Cell Counting in Fluorescent Microscopy through Deep Learning with c-ResUnet"
+    <https://www.nature.com/articles/s41598-021-01929-5>`_.
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+        progress (bool): If True, displays a progress bar of the download to stderr
+    """
+    return _resunet(
+        arch=arch,
+        n_features_start=n_features_start,
+        n_out=n_out,
+        pretrained=pretrained,
+        progress=progress,
+        **kwargs,
+    )
+
